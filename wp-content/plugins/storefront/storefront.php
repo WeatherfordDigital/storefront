6017dc9fed5e24ea86250e10fb769d9d755a3d90
Carla Chalmers <carla@northtexas.digital>
2023-08-29 07:53:18 -0500

    add ACF files to includes/acf

--- wp-content/plugins/storefront/storefront.php
+++ wp-content/plugins/storefront/storefront.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Storefront
- * Plugin URI:      PLUGIN SITE HERE
+ * Plugin URI:      https://weatherforddigital.com
  * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Author:          Carla Chalmers
+ * Author URI:      https://weatherforddigital.com
  * Text Domain:     storefront
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +12,23 @@
  * @package         Storefront
  */
 
-// Your code starts here.
+// Include ACF in plugin
+
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', plugin_dir_path(__FILE__) . '/includes/acf/' );
+define( 'MY_ACF_URL', plugin_dir_path(__FILE__) . '/includes/acf/' );
+
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+
+// (Optional) Hide the ACF admin menu item.
+add_filter('acf/settings/show_admin', '__return_false');
+
+// When including the PRO plugin, hide the ACF Updates menu
+add_filter('acf/settings/show_updates', '__return_false', 100);
